42591dd84e013dcae2f1c31d8e7deb67a2e02042
dionweb <dionysiostsentidis@gmail.com>
2020-03-08 18:08:58 +0200

    integrate tailwindcss

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,9 +4,20 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const tailwindcss = require("tailwindcss")
+
 module.exports = {
   siteName: "Sports News",
   plugins: [],
   siteUrl: "https://dionweb.github.io",
-  pathPrefix: "/sports-news"
+  pathPrefix: "/sports-news",
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss
+        ],
+      },
+    },
+  }
 };
